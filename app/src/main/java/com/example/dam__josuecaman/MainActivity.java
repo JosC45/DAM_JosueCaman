8999ab8deecd34a08df91c302da7c349b2aa71a4
Josue Caman <20b3010123@untels.edu.pe>
2024-05-15 10:48:27 -0500

    Parte3

--- app/src/main/java/com/example/dam__josuecaman/MainActivity.java
+++ app/src/main/java/com/example/dam__josuecaman/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.dam__josuecaman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Button enviar=findViewById(R.id.button_enviar);
+        enviar.setOnClickListener(view -> Toast.makeText(this, "Se envio sus datos", Toast.LENGTH_SHORT).show());
+
+
+
+}
 }
